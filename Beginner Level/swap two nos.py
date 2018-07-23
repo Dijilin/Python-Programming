fd77b1a961ba477c444cac7d4420c443bcf97569
Dijilin <36722779+Dijilin@users.noreply.github.com>
2018-07-23 11:18:07 +0530

    Update swap two nos.py

--- Beginner Level/swap two nos.py
+++ Beginner Level/swap two nos.py
@@ -1,7 +1,7 @@
-x = 5
-y = 10
-temp = x
-x = y
-y = temp
-print('The value of x after swapping: {}'.format(x))
-print('The value of y after swapping: {}'.format(y))
+a = 5
+b = 10
+temp = a
+a = b
+b = temp
+print('The value of x after swapping: {}'.format(a))
+print('The value of y after swapping: {}'.format(b))
