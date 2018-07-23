a19b0ff5f46e403bf9894a38407e366cd5e308f4
Dijilin <36722779+Dijilin@users.noreply.github.com>
2018-07-23 11:02:14 +0530

    Update count no of numeric characters.py

--- Beginner Level/count no of numeric characters.py
+++ Beginner Level/count no of numeric characters.py
@@ -1,9 +1,9 @@
 str=input('enter the string')
-count=0
+n=0
 
 for i in range(len(str)):
   if str[i].isdigit():
-    count=count+1
+    n=n+1
   else:
-    count=count
-print(count)
+    n=n
+print(n)
