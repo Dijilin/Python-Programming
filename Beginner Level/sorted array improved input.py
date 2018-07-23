112c3cbf271bcd50ecc47ef335116382ab9b1beb
Dijilin <36722779+Dijilin@users.noreply.github.com>
2018-07-23 14:11:50 +0530

    Update sorted array improved input.py

--- Beginner Level/sorted array improved input.py
+++ Beginner Level/sorted array improved input.py
@@ -1,8 +1,8 @@
-a=[]
+l=[]
 k=int(input("value"))
 for i in range(1,k+1):
  c=int(input("numbers"))
- a.append(c)
- a.sort()
-print(a)
-print(a[k//2])
+ l.append(c)
+ l.sort()
+print(l)
+print(l[k//2])
